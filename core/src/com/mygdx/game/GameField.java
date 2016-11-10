e03f2881ccbf7a3656f549f742445ac78e94c787
PeemN <4astralp@gmail.com>
2016-11-11 02:46:19 +0700

    box can move & drop to hole (has some bug)

--- core/src/com/mygdx/game/GameField.java
+++ core/src/com/mygdx/game/GameField.java
@@ -3,16 +3,21 @@ package com.mygdx.game;
 import java.util.Random;
 
 public class GameField {
-//mapcreator
+	public static int numberofHole = 6;
+	public static int numberofBox = 6;
+	public static int numberofRock = 6;
+	public static final int codeBlank = 0;
+	public static final int codeHole = 1;
+    public static final int codeBox = 2;
+    public static final int codeRock = 3;
+    public static final int DIRECTION_UP = 1;
+    public static final int DIRECTION_RIGHT = 2;
+    public static final int DIRECTION_DOWN = 3;
+    public static final int DIRECTION_LEFT = 4;
+	
 	public int[][] mapCreator() {
 		int Map[][] = new int[6][6];
-        int numberofHole = 6;
-        int numberofBox = 6;
-        int numberofRock = 6;
-        int codeHole = 1;
-        int codeBox = 2;
-        int codeRock = 3;
-        
+
         objectMarker(Map,numberofHole,codeHole);
         objectMarker(Map,numberofBox,codeBox);
         objectMarker(Map,numberofRock,codeRock);
@@ -23,7 +28,8 @@ public class GameField {
 		Random random = new Random();
 		int height = Map[1].length;
         int width = Map[0].length;
-		for (int i = 0; i < numberofObject; ) {
+        
+		for(int i = 0; i < numberofObject; ) {
 			int mapR = random.nextInt(height);
 			int mapC = random.nextInt(width);
 			if(Map[mapR][mapC] == 0){
@@ -33,4 +39,97 @@ public class GameField {
 		}
 		return Map;
 	}
+	
+	public int[][] mapUpdate(int[][] Map, int inputDirection) {
+		int[] currentBoxPositionX = findBoxX(Map, 6);
+		int[] currentBoxPositionY = findBoxY(Map, 6);
+        
+        for (int i = 0; i < numberofBox;i++){
+        	Map = checkNextBoxPosition(Map,currentBoxPositionX[i],currentBoxPositionY[i],inputDirection);
+        }
+		return Map;
+	}
+	
+	public int[] findBoxX(int[][] Map,int numberofBox){
+		int[] boxPositionX = new int[numberofBox];
+		int height = Map[1].length;
+        int width = Map[0].length;
+        int i = 0;
+        
+		for (int r = 0; r < height; r++) {
+    		for (int c = 0; c < width; c++) {
+    			if (Map[c][r] == 2){
+    				boxPositionX[i] = c;
+    				i++;
+    			}
+    		}
+		}
+			return boxPositionX;
+	}
+	
+	public int[] findBoxY(int[][] Map,int numberofBox){
+		int[] boxPositionY = new int[numberofBox];
+		int height = Map[1].length;
+        int width = Map[0].length;
+        int j = 0;
+        
+		for (int r = 0; r < height; r++) {
+    		for (int c = 0; c < width; c++) {
+    			if (Map[c][r] == 2){
+    				boxPositionY[j] = r;
+    				j++;
+    			}
+    		}
+		}
+			return boxPositionY;
+	}
+	public int[][] checkNextBoxPosition(int[][] Map,int column,int row, int movementtype){
+		int height = Map[1].length;
+        int width = Map[0].length;
+        int nextColumn = column;
+        int nextRow = row;
+        
+        switch (movementtype){
+        case DIRECTION_UP:
+        	nextRow += 1;
+        	break;
+        case DIRECTION_RIGHT:
+        	nextColumn += 1;
+        	break;
+        case DIRECTION_DOWN:
+        	nextRow -= 1;
+        	break;
+        case DIRECTION_LEFT:
+        	nextColumn -= 1;
+        	break;
+        }
+		if (nextColumn < 0){
+			nextColumn = 0;
+		}
+		if (nextColumn > width){
+			nextColumn = width -1;
+		}
+		if (nextRow < 0){
+			nextRow = 0;
+		}
+		if (nextRow > height){
+			nextRow = height -1;
+		}
+		switch (Map[nextColumn][nextRow]){
+		case codeBlank:
+			Map[column][row] = 0;
+			Map[nextColumn][nextRow] = 2;
+            break;
+		case codeBox:
+            break;
+        case codeRock:
+            break;
+		case codeHole:
+			Map[column][row] = 0;
+			Map[nextColumn][nextRow] = 0;
+			break;
+		}
+		return Map;
+	}
+		
 }
--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -17,8 +17,7 @@ public class GameScreen extends ScreenAdapter {
 	private SpriteBatch batch;
 	private BitmapFont font;
 	private ToDB toDB;
-    private int boxPositionX;
-    private int boxPositionY;
+	private int inputDirection;
     private int height;
     private int width;
     private int stageCounter;
@@ -29,11 +28,10 @@ public class GameScreen extends ScreenAdapter {
     private Texture boxImg;
     private Texture rockImg;
     private int[][] gameboard;
-    private GameField gameField;
+    GameField gameField = new GameField();
     
 	public GameScreen(ToDB toDB) {
         this.toDB = toDB;
-        GameField gameField = new GameField();
         
 		status = new Texture("statusboard.jpg");
 		holeImg = new Texture("testhole.jpg");
@@ -41,18 +39,18 @@ public class GameScreen extends ScreenAdapter {
 		rockImg = new Texture("testrock.jpg");
 		font = new BitmapFont();
 	    font.setColor(Color.WHITE);
-        boxPositionX = 0;
-        boxPositionY = 0;
+	    inputDirection = 0;
         stageCounter = 0;
         turn = 60;
     }
 
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
 	}
 	
 	@Override
-	public void render (float delta) {
+	public void render(float delta) {
+		
 		update(delta);
 		SpriteBatch batch = toDB.batch;
         Gdx.gl.glClearColor(0.85f, 0.85f, 0.85f, 1);
@@ -75,7 +73,7 @@ public class GameScreen extends ScreenAdapter {
         			if (gameboard[c][r] == 1) {
         				batch.draw(holeImg, x, y);
         			} else if (gameboard[c][r] == 2) {
-        				batch.draw(boxImg, x + boxPositionX, y + boxPositionY);
+        				batch.draw(boxImg, x, y);
         			} else if (gameboard[c][r] == 3) {
         				batch.draw(rockImg, x, y);
         			}
@@ -84,54 +82,25 @@ public class GameScreen extends ScreenAdapter {
         	batch.draw(status, 0, 600);
         	System.out.println(turn);
         	batch.end();
+        	gameboard = gameField.mapUpdate(gameboard, inputDirection);
         	
 	}
-    
-	/*public int[][] mapCreator() {
-		int Map[][] = new int[6][6];
-        int numberofHole = 6;
-        int numberofBox = 6;
-        int numberofRock = 6;
-        int codeHole = 1;
-        int codeBox = 2;
-        int codeRock = 3;
-        
-        objectMarker(Map,numberofHole,codeHole);
-        objectMarker(Map,numberofBox,codeBox);
-        objectMarker(Map,numberofRock,codeRock);
-		return Map;
-	}
-	
-	public int[][] objectMarker(int[][] Map, int numberofObject, int typeofObject) {
-		Random random = new Random();
-		int height = Map[1].length;
-        int width = Map[0].length;
-		for (int i = 0; i < numberofObject; ) {
-			int mapR = random.nextInt(height);
-			int mapC = random.nextInt(width);
-			if(Map[mapR][mapC] == 0){
-				Map[mapR][mapC] = typeofObject;
-				i++;
-			}
-		}
-		return Map;
-	}*/
 	
 	private void update(float delta) {
         if (Gdx.input.isKeyPressed(Keys.UP)) {
-        	boxPositionY += 10;
-        	turn--;
-        }
-        if (Gdx.input.isKeyPressed(Keys.LEFT)) {
-        	boxPositionX -= 10;
+        	inputDirection = 1;
         	turn--;
         }
         if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
-        	boxPositionX += 10;
+        	inputDirection = 2;
         	turn--;
         }
         if (Gdx.input.isKeyPressed(Keys.DOWN)) {
-        	boxPositionY -= 10;
+        	inputDirection = 3;
+        	turn--;
+        }
+        if (Gdx.input.isKeyPressed(Keys.LEFT)) {
+        	inputDirection = 4;
         	turn--;
         }
     }
